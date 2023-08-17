0068887b30a234792eefeec6bdde056f267f351c
sebamarambio96 <s.marambiosandoval@gmail.com>
2023-08-17 18:00:24 -0400

    Primer commit en la rama main

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
 });
+ */
+
+/* Route::view('/', 'welcome')->name('welcome'); */
+Route::view('/about', 'landing.about')->name('about');
+Route::view('/', 'landing.index')->name('index');
+
+
+/* Route::get();
+Route::put();
+Route::delete();
+Route::patch(); */
