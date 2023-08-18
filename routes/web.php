aad2aa4053f9ceaabaff970a82934c873891cce7
sebamarambio96 <s.marambiosandoval@gmail.com>
2023-08-18 12:10:12 -0400

    s

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,10 @@ use Illuminate\Support\Facades\Route;
  */
 
 /* Route::view('/', 'welcome')->name('welcome'); */
-Route::view('/about', 'landing.about')->name('about');
-Route::view('/', 'landing.index')->name('index');
+Route::view('/', 'index')->name('index');
+Route::view('/about', 'about')->name('about');
+Route::view('/services', 'services')->name('services');
+Route::view('/contact', 'contact')->name('contact');
 
 
 /* Route::get();
